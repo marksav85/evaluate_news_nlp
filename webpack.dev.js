564b942a691fe5cebcaf03312f3cb2d0af73612e
marksav85 <mark.saville@outlook.com>
2021-04-10 20:44:39 +0200

    refactor: Minor changes to assist with dev-mode bug

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,19 +1,29 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 
 
 module.exports = {
     entry: './src/client/index.js',
+    mode: 'development',
+    devtool: 'source-map',
+    stats: 'verbose',
+    /*devServer: {
+        host: 'localhost',
+        port: 8080,
+        proxy: {
+          context: () => true,
+          target: 'http://localhost:8090',
+          secure: false
+        }
+    },*/
     output: {
         libraryTarget: 'var',
         library: 'Client',
         clean: true, // Clean the output directory before emit.
     },
-    mode: 'development',
-    devtool: 'source-map',
-    stats: 'verbose',
     module: {
         rules: [
             {
@@ -31,7 +41,7 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html"
-        })
-
+        }),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
